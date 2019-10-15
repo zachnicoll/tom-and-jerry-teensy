97e149e82deff8bcca141205345ad2dcdbde284c
Zach Nicoll <zaxternoob@gmail.com>
2019-10-15 12:27:59 +1000

    Developing cheese and trap placement

--- src/tomjerry.c
+++ src/tomjerry.c
@@ -18,8 +18,8 @@
 #define NUM_SWITCHES 7
 #define PLR_WIDTH 5
 #define PLR_HEIGHT 4
+#define OBJ_SIZE 5
 #define MINSPEED 0.2
-
 
 // Jerry Bitmap
 uint8_t jerry_bitmap[PLR_HEIGHT][PLR_WIDTH] = {{1, 1, 0, 1, 1}, {1, 1, 0, 1, 1}, {1, 1, 1, 1, 1}, {0, 1, 1, 1, 0}};
@@ -27,10 +27,21 @@ uint8_t jerry_bitmap[PLR_HEIGHT][PLR_WIDTH] = {{1, 1, 0, 1, 1}, {1, 1, 0, 1, 1},
 // Tom Bitmap
 uint8_t tom_bitmap[PLR_HEIGHT][PLR_WIDTH] = {{0, 1, 0, 1, 0}, {1, 1, 1, 1, 1}, {1, 0, 1, 0, 1}, {0, 1, 1, 1, 0}};
 
+// Cheese Bitmap
+uint8_t cheese_bitmap[OBJ_SIZE][OBJ_SIZE] = {{1, 1, 1, 1, 1}, {1, 0, 0, 0, 1}, {1, 0, 1, 1, 1}, {1, 0, 0, 0, 1}, {1, 1, 1, 1, 1}};
+
+// Trap Bitmap
+uint8_t trap_bitmap[OBJ_SIZE][OBJ_SIZE] = {{1, 1, 1, 1, 1}, {1, 0, 0, 0, 1}, {1, 1, 0, 1, 1}, {1, 1, 0, 1, 1}, {1, 1, 1, 1, 1}};
+
+// Door Bitmap
+uint8_t door_bitmap[OBJ_SIZE][OBJ_SIZE] = {{1, 1, 1, 1, 1}, {1, 0, 0, 0, 1}, {1, 0, 1, 0, 1}, {1, 0, 0, 0, 1}, {1, 1, 1, 1, 1}};
+
 // Global Vars
-int current_level = 1;
+int current_level = 1, cheese, cheese_collected, cheese_time, traps, trap_supply, trap_time;
+int cheese_positions[5][2], trap_positions[5][2], door_position[2];
 bool pause = false;
 bool game_over = false;
+bool pause_check = false;
 
 struct player
 {
@@ -46,6 +57,11 @@ struct wall
 volatile uint8_t state_counts[7];
 volatile uint8_t switch_states[7];
 // [SW1, SW2, SWA, SWB, SWC, SWD, SWCENTER]
+volatile uint32_t cycle_count = 0;
+
+// Fucntion Declarations
+bool check_collision(struct player plyr, double dx, double dy, char obj);
+void elapsed_time();
 
 void start_screen(void)
 {
@@ -110,6 +126,17 @@ void setup_vars(void)
         state_counts[i] = 0b00000000;
         switch_states[i] = 0b00000000;
     }
+
+    for (int i = 0; i < 5; i++)
+    {
+        cheese_positions[i][0] = -10;
+        cheese_positions[i][1] = -10;
+        trap_positions[i][0] = -10;
+        trap_positions[i][1] = -10;
+    }
+
+    cheese_time = elapsed_time();
+    trap_time = elapsed_time();
 }
 
 void setup(void)
@@ -142,24 +169,35 @@ void setup(void)
 
     // Setup Timers
 
-    // Timer 0, normal mode, 0.08s overflow
+    // Timer 0 (debouncer), normal mode, 0.08s overflow
     TCCR0A = 0;
     TCCR0B = 4;
     TIMSK0 = 1; // Enable  overflow interupts for this timer.
+
+    // Timer 1 (usb_serial inputs), normal mode, 0.08s overflow
+    TCCR1A = 0;
+    TCCR1B = 4;
+    TIMSK1 = 1; // Enable  overflow interupts for this timer.
 
     // Enable interupts
     sei();
 
     // Enable USB Serial
     usb_init();
-    while ( !usb_configured() ) {
-		// Block until USB is ready.
-	}
+    while (!usb_configured())
+    {
+        // Block until USB is ready.
+    }
 
     // Set Initial Var Values
     setup_vars();
 
     start_screen();
+
+    // Timer 3 (game time), normal mode, 1s overflow
+    TCCR3A = 0;
+    TCCR3B = 1;
+    TIMSK3 = 1; // Enable  overflow interupts for this timer.
 }
 
 // Interrupts
@@ -170,7 +208,7 @@ ISR(TIMER0_OVF_vect)
     for (int i = 0; i < NUM_SWITCHES; i++)
     {
         state_counts[i] = state_counts[i] << 1;
-        uint8_t mask = 0b00001111;
+        uint8_t mask = 0b00011111;
         state_counts[i] &= mask;
         state_counts[i] |= BIT_IS_SET(pin_arr_1[i], pin_arr_2[i]);
 
@@ -183,6 +221,41 @@ ISR(TIMER0_OVF_vect)
             switch_states[i] = 0;
         }
     }
+}
+
+ISR(TIMER1_OVF_vect)
+{
+    int16_t c = usb_serial_getchar();
+
+    // Down
+    if (c == 's' && jerry.y + PLR_HEIGHT + 1 < LCD_Y && !check_collision(jerry, 0, 1, 'W'))
+    {
+        jerry.y++;
+    }
+    // Left
+    else if (c == 'a' && jerry.x - 1 > 0 && !check_collision(jerry, -1, 0, 'W'))
+    {
+        jerry.x--;
+    }
+    // Up
+    else if (c == 'w' && jerry.y - 1 > STATUS_BAR_HEIGHT && !check_collision(jerry, 0, -1, 'W'))
+    {
+        jerry.y--;
+    } // Right
+    else if (c == 'd' && jerry.x + 1 + PLR_WIDTH < LCD_X && !check_collision(jerry, 1, 0, 'W'))
+    {
+        jerry.x++;
+    }
+}
+
+ISR(TIMER3_OVF_vect)
+{
+    cycle_count++;
+}
+
+double elapsed_time()
+{
+    return (cycle_count * 65536.0 + TCNT3) * (1.0 / 8000000.0);
 }
 
 //SETUP INTERUPT FOR GAME TIME TIMER
@@ -201,8 +274,12 @@ void draw_gui(void)
     draw_string(36, 0, str_buffer, FG_COLOUR);
 
     // FIX TIME!!!!!!!!!!!!!! USING TIMER INTERUPT AT 1 SECOND
-    //sprintf(str_buffer, "T:%d", "00:00");
-    draw_string(55, 0, "00:00", FG_COLOUR);
+    int i_minutes = floor(elapsed_time() / 60.0);
+    double fl_minutes = elapsed_time() / 60.0;
+    double fraction = fl_minutes - floor(fl_minutes);
+    int seconds = 60.0 * fraction;
+    sprintf(str_buffer, "%02d:%02d", i_minutes, seconds);
+    draw_string(55, 0, str_buffer, FG_COLOUR);
 
     draw_line(0, STATUS_BAR_HEIGHT, LCD_X, STATUS_BAR_HEIGHT, FG_COLOUR);
 }
@@ -249,14 +326,53 @@ void draw_walls(void)
 
 void draw_objs(void)
 {
+    for (int i = 0; i < 5; i++)
+    {
+        for (int j = 0; j < OBJ_SIZE; j++)
+        {
+            for (int k = 0; k < OBJ_SIZE; k++)
+            {
+                if (cheese_positions[i][0] != -10)
+                {
+                    if (cheese_bitmap[j][k] == 1)
+                    {
+                        draw_pixel(cheese_positions[i][0] + j, cheese_positions[i][1] + k, FG_COLOUR);
+                    }
+                }
+
+                if (trap_positions[i][0] != -10)
+                {
+                    if (trap_bitmap[j][k] == 1)
+                    {
+                        draw_pixel(trap_positions[i][0] + j, trap_positions[i][1] + k, FG_COLOUR);
+                    }
+                }
+            }
+        }
+    }
 }
 
 void draw(void)
 {
     draw_gui();
-    draw_tom();
     draw_jerry();
+    draw_tom();
+
     draw_objs();
+}
+
+bool is_pixel(int x, int y)
+{
+    uint8_t bank = y >> 3;
+    uint8_t pixel = y & 7;
+
+    if (((screen_buffer[bank * LCD_X + (int)x] >> pixel) & 1) == 1)
+    {
+        return true;
+    }
+    else{
+        return false;
+    }
 }
 
 bool wall_collision(struct player plyr, int dx, int dy)
@@ -297,16 +413,16 @@ bool wall_collision(struct player plyr, int dx, int dy)
     return false;
 }
 
-bool tom_collision(void)
+bool tom_collision(int dx, int dy)
 {
     // If one rectangle is on left side of other
-    if (jerry.x > tom.x + PLR_WIDTH || tom.x > jerry.x + PLR_WIDTH)
+    if (jerry.x + dx > tom.x + PLR_WIDTH || tom.x > jerry.x + dx + PLR_WIDTH)
     {
         return false;
     }
 
     // If one rectangle is above other
-    if (jerry.y > tom.y + PLR_HEIGHT || tom.y > jerry.y + PLR_HEIGHT)
+    if (jerry.y + dy > tom.y + PLR_HEIGHT || tom.y > jerry.y + dy + PLR_HEIGHT)
     {
         return false;
     }
@@ -343,37 +459,63 @@ bool check_collision(struct player plyr, double dx, double dy, char obj)
     return collided;
 }
 
+void randomize_tom()
+{
+    tom.speed = (double)rand() / (double)RAND_MAX * MINSPEED + MINSPEED;
+    tom.direction = ((double)rand() / (double)RAND_MAX) * M_PI * 2;
+}
+
 void handle_player(void)
 {
-    if (tom_collision())
+    int dx = 0;
+    int dy = 0;
+
+    // Down
+    if (switch_states[2] == 1)
+    {
+        dy = 1;
+    }
+    // Left
+    else if (switch_states[3] == 1)
+    {
+        dx = -1;
+    }
+    // Up
+    else if (switch_states[4] == 1)
+    {
+        dy = -1;
+    } // Right
+    else if (switch_states[5] == 1)
+    {
+        dx = 1;
+    }
+
+    if (!tom_collision(dx, dy))
+    {
+        if (jerry.x + dx + PLR_WIDTH < LCD_X && jerry.x + dx > 0 && jerry.y + dy + PLR_HEIGHT < LCD_Y && jerry.y + dy > STATUS_BAR_HEIGHT && !check_collision(jerry, dx, dy, 'W'))
+        {
+            jerry.x += dx;
+            jerry.y += dy;
+        }
+    }
+    else
     {
         jerry.x = jerry.init_x;
         jerry.y = jerry.init_y;
         jerry.lives--;
         tom.x = tom.init_x;
         tom.y = tom.init_y;
+        randomize_tom();
     }
-    else
+
+    if (switch_states[0] == 1 && pause_check == false)
     {
-        // Down
-        if (switch_states[2] == 1 && jerry.y + PLR_HEIGHT + 1 < LCD_Y && !check_collision(jerry, 0, 1, 'W'))
-        {
-            jerry.y++;
-        }
-        // Left
-        else if (switch_states[3] == 1 && jerry.x - 1 > 0 && !check_collision(jerry, -1, 0, 'W'))
-        {
-            jerry.x--;
-        }
-        // Up
-        else if (switch_states[4] == 1 && jerry.y - 1 > STATUS_BAR_HEIGHT && !check_collision(jerry, 0, -1, 'W'))
-        {
-            jerry.y--;
-        } // Right
-        else if (switch_states[5] == 1 && jerry.x + 1 + PLR_WIDTH < LCD_X && !check_collision(jerry, 1, 0, 'W'))
-        {
-            jerry.x++;
-        }
+        pause = !pause;
+        pause_check = true;
+    }
+    else if (switch_states[0] == 0 && pause_check == true)
+    {
+        pause_check = false;
     }
 }
 
@@ -386,8 +528,7 @@ void update_enemy(void)
 
     if ((tom.x + dx + (PLR_WIDTH * xdir) > LCD_X) || (tom.x + dx < 0) || (tom.y + dy + (PLR_HEIGHT * ydir) > LCD_Y) || (tom.y + dy < STATUS_BAR_HEIGHT + 1) || check_collision(tom, dx, dy, 'W') || check_collision(tom, dx, 0, 'W') || check_collision(tom, 0, dy, 'W'))
     {
-        tom.speed = (double)rand() / (double)RAND_MAX * MINSPEED + MINSPEED;
-        tom.direction = ((double)rand() / (double)RAND_MAX) * M_PI * 2;
+        randomize_tom();
     }
     else
     {
@@ -403,17 +544,66 @@ void update_enemy(void)
     }
 }
 
+void place_cheese_traps()
+{
+    int current_time = round(elapsed_time());
+
+    if (cheese < 5 && current_time - cheese_time == 2 && !pause)
+    {
+        place_cheese();
+    }
+    else if (cheese == 5 || pause)
+    {
+        cheese_time = round(elapsed_time());
+    }
+
+    if (trap_supply > 0 && current_time - trap_time == 3 && !pause)
+    {
+        place_trap();
+    }
+    else if (traps == 5 || pause)
+    {
+        trap_time = round(elapsed_time());
+    }
+}
+
+void place_cheese()
+{
+    int x, y;
+    x = round(((double)rand() / (double)RAND_MAX) * (LCD_X - 1));
+    y = round(((double)rand() / (double)RAND_MAX) * (LCD_Y - 4)) + 4;
+
+    if (!is_pixel(x,y)))
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            if (cheese_positions[i][0] == -1)
+            {
+                cheese_positions[i][0] = x;
+                cheese_positions[i][1] = y;
+                cheese++;
+                break;
+            }
+        }
+    }
+}
+
+void place_trap()
+{
+}
+
 void process(void)
 {
     clear_screen();
     draw_walls();
-    update_enemy();
+    if (!pause)
+    {
+        update_enemy();
+    }
     draw();
     handle_player();
+    place_cheese();
     show_screen();
-
-    usb_serial_putchar('#');    
-
     srand(TCNT0);
 }
 
